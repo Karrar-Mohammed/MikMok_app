9efcbd125aa83e824919926e0b484e1eecf2a769
aya155 <ayaalnaqeep@gmail.com>
2022-07-31 02:21:06 +0300

    add splash view and app icon with change in color file

--- app/src/main/java/com/example/mikmok_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/mikmok_app/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.mikmok_app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.mikmok_app.R
 import com.example.mikmok_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
