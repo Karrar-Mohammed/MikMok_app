62adef1231eaa080e02d8912d5c0f62f4764b806
shamardn <shamardn@gmail.com>
2022-07-30 22:35:03 +0200

    stop scrolling to view one item every time scrolling

--- app/src/main/java/com/example/mikmok_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/mikmok_app/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.mikmok_app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mikmok_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +20,30 @@ class MainActivity : AppCompatActivity() {
     private fun setup(){
         val adapter = VideoPlayerAdapter(DataManager.getVideoPlayer())
         binding.recyclerview.adapter = adapter
+
+        val snapHelper = object : LinearSnapHelper() {
+            override fun findTargetSnapPosition(
+                layoutManager: RecyclerView.LayoutManager,
+                velocityX: Int,
+                velocityY: Int,
+            ): Int {
+                val centerView = findSnapView(layoutManager) ?: return RecyclerView.NO_POSITION
+                val position = layoutManager.getPosition(centerView)
+                var targetPosition = -1
+
+                if (layoutManager.canScrollVertically()) {
+                    targetPosition = if (velocityY < 0) {
+                        position - 1
+                    } else {
+                        position + 1
+                    }
+                }
+                val firstItem = 0
+                val lastItem = layoutManager.itemCount - 1
+                targetPosition = Math.min(lastItem, Math.max(targetPosition, firstItem))
+                return targetPosition
+            }
+        }
+        snapHelper.attachToRecyclerView(binding.recyclerview)
     }
 }
