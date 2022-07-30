21027a4962c9a72731b5efeea3d3db8797c53ea8
Rasha Alaswad <eng.rns@hotmail.com>
2022-07-31 02:12:39 +0300

    fix: remove parsing operation from main activity

---
+++ app/src/main/java/com/example/mikmok_app/data/network/RequestUsingOkHTTP.kt
@@ -0,0 +1,36 @@
+package com.example.mikmok_app.data.network
+
+import android.util.Log
+import com.example.mikmok_app.data.ClassicItem
+import com.example.mikmok_app.data.Item
+import com.example.mikmok_app.util.Constants
+import com.example.mikmok_app.util.DataManager
+import com.google.gson.Gson
+import okhttp3.*
+import java.io.IOException
+
+class RequestUsingOkHTTP {
+    private var _responseItemList = mutableListOf<Item>()
+    val items: List<Item> get() = _responseItemList
+
+    fun makeRequestUsingOkHTTP() {
+        val client = OkHttpClient()
+        val request = Request.Builder().url(Constants.URL).build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("TAG", "onFailure: ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.string().let { jsonString ->
+                    val result = Gson().fromJson(jsonString, ClassicItem::class.java)
+                    result.feed.forEach { item ->
+                        _responseItemList = DataManager().setVideoList(item)
+                        Log.d("TAG", "onResponse: $items")
+                    }
+                }
+            }
+        }
+        )
+    }
+}
--- app/src/main/java/com/example/mikmok_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/mikmok_app/ui/MainActivity.kt
@@ -6,58 +6,25 @@ import android.util.Log
 import com.example.mikmok_app.data.ClassicItem
 import com.example.mikmok_app.data.Feed
 import com.example.mikmok_app.data.Item
+import com.example.mikmok_app.data.network.RequestUsingOkHTTP
 import com.example.mikmok_app.databinding.ActivityMainBinding
 import com.example.mikmok_app.util.Constants
+import com.example.mikmok_app.util.DataManager
 import com.google.gson.Gson
 import okhttp3.*
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val client = OkHttpClient()
-    private val videosList = mutableListOf<Item>()
+    private var videosList = listOf<Item>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        makeRequestUsingOkHTTP()
-    }
-
-    private fun makeRequestUsingOkHTTP() {
-        val request = Request.Builder().url(Constants.URL).build()
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Log.d("TAG", "onFailure: ${e.message}")
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                response.body?.string().let { jsonString ->
-                    val result = Gson().fromJson(jsonString, ClassicItem::class.java)
-                    result.feed.forEach { item ->
-                        setVideoList(item)
-                        Log.d("videosList", videosList.first().toString())
-                    }
-                }
-            }
-        })
-    }
-
-    private fun setVideoList(item: Feed): List<Item> {
-        item.items.forEach {
-            val item = Item(
-                id = it.id,
-                year = it.year,
-                title = it.title,
-                description = it.description,
-                duration = it.duration,
-                director = it.director,
-                url = it.url,
-                art = it.director
-            )
-            videosList.add(item)
-        }
-        return videosList
+        RequestUsingOkHTTP().makeRequestUsingOkHTTP()
+        videosList = RequestUsingOkHTTP().items
+        Log.d("MainActivity", "onCreate: ${videosList}")
     }
 }
