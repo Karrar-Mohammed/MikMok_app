63796e291d096377a27ae492b9f0032d1e243aaa
omar ezzdeen <omar.izziddeen@gmail.com>
2022-07-31 00:20:53 +0300

    create home view ui

--- app/src/main/java/com/example/mikmok_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/mikmok_app/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.mikmok_app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import com.example.mikmok_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Remove status bar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setup()
