c0f2bad6e0c8d3ac5dca0c4731c0453f29021e65
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2022-07-31 18:06:59 +0300

    change status bar color

--- app/src/main/java/com/example/mikmok_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/mikmok_app/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mikmok_app.ui
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.PagerSnapHelper
 import com.example.mikmok_app.data.DataManager
 import com.example.mikmok_app.data.network.Client
 import com.example.mikmok_app.databinding.ActivityMainBinding
@@ -21,9 +23,7 @@ class MainActivity : AppCompatActivity() {
     private fun setup() {
         val adapter = VideoPlayerAdapter(dataManager.getVideoPlayer())
         binding.recyclerview.adapter = adapter
-
         PagerSnapHelper().attachToRecyclerView(binding.recyclerview)
-
 
         val okHTTP = Client(dataManager)
         okHTTP.getFilmsList()
