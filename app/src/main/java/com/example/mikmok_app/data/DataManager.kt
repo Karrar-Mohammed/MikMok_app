6791cd1888ee077d8cf9938d62e81d4ccfa1e583
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-08-02 00:35:33 +0300

    delete unnecessary data

--- app/src/main/java/com/example/mikmok_app/data/DataManager.kt
+++ app/src/main/java/com/example/mikmok_app/data/DataManager.kt
@@ -1,7 +1,6 @@
 package com.example.mikmok_app.data
 
 import com.example.mikmok_app.data.domain.Film
-import com.example.mikmok_app.data.domain.VideoPlayer
 
 class DataManager {
 
@@ -13,18 +12,4 @@ class DataManager {
         _filmsList.add(film)
     }
 
-    fun getVideoPlayer(): List<VideoPlayer> = listOf(
-        VideoPlayer("Life is hard but beautiful","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less"),
-        VideoPlayer("nature is beautiful","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"),
-        VideoPlayer("Life is hard but beautiful","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less"),
-        VideoPlayer("nature is beautiful","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"),
-        VideoPlayer("Life is hard but beautiful","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less"),
-        VideoPlayer("nature is beautiful","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"),
-        VideoPlayer("Life is hard but beautiful","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less"),
-        VideoPlayer("nature is beautiful","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"),
-        VideoPlayer("Life is hard but beautiful","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less"),
-        VideoPlayer("nature is beautiful","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"),
-        VideoPlayer("Life is hard but beautiful","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less"),
-        VideoPlayer("nature is beautiful","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"),
-    )
 }
