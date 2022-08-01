6db83355cf61023ad16db3080b02e2f562f5a2e0
AmeerAmjed <ameer>
2022-08-01 16:13:30 +0300

    implement exoplayer

--- app/src/main/java/com/example/mikmok_app/ui/DataManager.kt
+++ app/src/main/java/com/example/mikmok_app/ui/DataManager.kt
@@ -4,6 +4,7 @@ import com.example.mikmok_app.ui.domain.VideoPlayer
 
 object DataManager {
     fun getVideoPlayer(): List<VideoPlayer> = listOf(
+//        VideoPlayer("Life is hard but beautiful","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less","https://android-tv-classics.firebaseapp.com/content/buffalo_dance/media_buffalo_dance.mp4"),
         VideoPlayer("Life is hard but beautiful","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less","https://android-tv-classics.firebaseapp.com/content/patchwork_girl_oz/media_patchwork_girl_oz.mp4"),
         VideoPlayer("nature is beautiful","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,","https://android-tv-classics.firebaseapp.com/content/tarzan_1918/media_tarzan_1918.mp4"),
     )
--- app/src/main/java/com/example/mikmok_app/ui/VideoPlayerAdapter.kt
+++ app/src/main/java/com/example/mikmok_app/ui/VideoPlayerAdapter.kt
@@ -1,46 +1,78 @@
 package com.example.mikmok_app.ui
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import com.example.mikmok_app.R
 import com.example.mikmok_app.databinding.ItemVideoPlayerBinding
 import com.example.mikmok_app.ui.domain.VideoPlayer
-import com.google.android.exoplayer2.*
-import com.google.android.exoplayer2.ui.PlayerView
+import com.example.mikmok_app.ui.util.istVisible
+import com.example.mikmok_app.ui.util.showMessage
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
+
 
 class VideoPlayerAdapter(
-    private val list: List<VideoPlayer>
-    ): RecyclerView.Adapter<VideoPlayerAdapter.VideoPlayerViewHolder>() {
+    private val list: List<VideoPlayer>,
+) : RecyclerView.Adapter<VideoPlayerAdapter.VideoPlayerViewHolder>() {
 
-
-    private lateinit var simpleExoPlayer:ExoPlayer
-    private lateinit var playerView: PlayerView
+    public lateinit var exoPlayer: ExoPlayer
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoPlayerViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(com.example.mikmok_app.R.layout.item_video_player, parent, false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_video_player, parent, false)
         return VideoPlayerViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: VideoPlayerViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: VideoPlayerViewHolder,
+        position: Int,
+    ) {
         holder.binding.apply {
             textTitle.text = list[position].videoTitle
             textDescription.text = list[position].videoDescription
-                    simpleExoPlayer = ExoPlayer.Builder(holder.binding.root.context).build()
-        playerView = videoPlayerss
-        playerView.player = simpleExoPlayer
-        val mediaItem: MediaItem = MediaItem.fromUri(list[position].video)
-        simpleExoPlayer.addMediaItem(mediaItem)
-        simpleExoPlayer.prepare()
 
-        simpleExoPlayer.playWhenReady = true
+            exoPlayer = ExoPlayer.Builder(holder.binding.root.context).build()
+            exoPlayer.addListener(object : Player.Listener {
 
+                override fun onPlaybackStateChanged(playbackState: Int) {
+                    when (playbackState) {
+                        Player.STATE_BUFFERING -> loadingVideo.visibility = istVisible(true)
+                        Player.STATE_READY -> loadingVideo.visibility = istVisible(false)
+                        Player.STATE_ENDED -> {
+                            exoPlayer.seekTo(0)
+                            exoPlayer.play()
+                        }
+                        else -> loadingVideo.visibility = istVisible(true)
+                    }
+                }
+
+                override fun onPlayerError(error: PlaybackException) {
+                    showMessage(holder.binding.root.context, R.string.error_show_video)
+                }
+            })
+            videoPlayerss.player = exoPlayer
+            videoPlayerss.useController = false
+            exoPlayer.seekTo(0)
+
+            val mediaItem: MediaItem = MediaItem.fromUri(list[position].video)
+
+            exoPlayer.addMediaItem(mediaItem)
+            exoPlayer.prepare()
+            exoPlayer.playWhenReady = true
+            exoPlayer.play()
         }
+
     }
 
     override fun getItemCount(): Int = list.size
 
-    class VideoPlayerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class VideoPlayerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val binding = ItemVideoPlayerBinding.bind(itemView)
     }
 }
