e7b26d37daf16035f319d062cb6eb26bcd36efe8
AmeerAmjed <ameer>
2022-08-01 20:57:26 +0300

    cleanup and create class ExoPlayerVideo

--- app/src/main/java/com/example/mikmok_app/ui/VideoPlayerAdapter.kt
+++ app/src/main/java/com/example/mikmok_app/ui/VideoPlayerAdapter.kt
@@ -1,27 +1,20 @@
 package com.example.mikmok_app.ui
 
-import android.net.Uri
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.RequestManager
 import com.example.mikmok_app.R
 import com.example.mikmok_app.databinding.ItemVideoPlayerBinding
 import com.example.mikmok_app.ui.domain.VideoPlayer
-import com.example.mikmok_app.ui.util.istVisible
-import com.example.mikmok_app.ui.util.showMessage
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.PlaybackException
-import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.ui.PlayerView
 
 
-class VideoPlayerAdapter(
-    private val list: List<VideoPlayer>,
-) : RecyclerView.Adapter<VideoPlayerAdapter.VideoPlayerViewHolder>() {
+class VideoPlayerAdapter(private val list: List<VideoPlayer>) : RecyclerView.Adapter<VideoPlayerAdapter.VideoPlayerViewHolder>() {
 
-    public lateinit var exoPlayer: ExoPlayer
+    private lateinit var exoPlayer: ExoPlayer
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoPlayerViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -36,38 +29,26 @@ class VideoPlayerAdapter(
         holder.binding.apply {
             textTitle.text = list[position].videoTitle
             textDescription.text = list[position].videoDescription
-
-            exoPlayer = ExoPlayer.Builder(holder.binding.root.context).build()
-            exoPlayer.addListener(object : Player.Listener {
-
-                override fun onPlaybackStateChanged(playbackState: Int) {
-                    when (playbackState) {
-                        Player.STATE_BUFFERING -> loadingVideo.visibility = istVisible(true)
-                        Player.STATE_READY -> loadingVideo.visibility = istVisible(false)
-                        Player.STATE_ENDED -> {
-                            exoPlayer.seekTo(0)
-                            exoPlayer.play()
-                        }
-                        else -> loadingVideo.visibility = istVisible(true)
-                    }
-                }
-
-                override fun onPlayerError(error: PlaybackException) {
-                    showMessage(holder.binding.root.context, R.string.error_show_video)
-                }
-            })
-            videoPlayerss.player = exoPlayer
-            videoPlayerss.useController = false
-            exoPlayer.seekTo(0)
-
-            val mediaItem: MediaItem = MediaItem.fromUri(list[position].video)
-
-            exoPlayer.addMediaItem(mediaItem)
-            exoPlayer.prepare()
-            exoPlayer.playWhenReady = true
-            exoPlayer.play()
+            setExoPlayer(holder,videoPlayerss,root.context,position)
         }
 
+    }
+
+    private fun setExoPlayer(
+        holder: VideoPlayerViewHolder,
+        videoPlayers: PlayerView,
+        context: Context,
+        position: Int
+    ) {
+        exoPlayer = ExoPlayerVideo(holder).initialization()
+        videoPlayers.player = exoPlayer
+        videoPlayers.useController = false
+        exoPlayer.seekTo(0)
+        val mediaSource = ExoPlayerVideo.mediaSource(context, list[position].video)
+        exoPlayer.setMediaSource(mediaSource)
+        exoPlayer.prepare()
+        exoPlayer.playWhenReady = true
+        exoPlayer.play()
     }
 
     override fun getItemCount(): Int = list.size
